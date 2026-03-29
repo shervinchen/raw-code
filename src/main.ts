bd0581c9235821dc66428a243daeb4f0dd05c5aa
shervinchen <shervinchen@gmail.com>
2026-03-29 15:10:50 +0800

    ✨ feat: read ollama data and output

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-console.log("Hello, World!");
+const res = await fetch("http://localhost:11434/api/generate", {
+  method: "POST",
+  body: JSON.stringify({
+    model: "qwen3:4b-instruct-2507-q4_K_M",
+    prompt: "why is the sky blue?",
+    // stream: false,
+  }),
+});
+
+if (!res.body) process.exit(0);
+
+const decoderStream = new TextDecoderStream();
+
+const stream = res.body.pipeThrough(decoderStream);
+
+for await (const chunk of stream) {
+  const json = JSON.parse(chunk);
+  process.stdout.write(json.response);
+}
+
+process.stdout.write("\n");
