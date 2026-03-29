065a77cd8295f15b11e5552fb7da970c6f03143d
shervinchen <shervinchen@gmail.com>
2026-03-29 16:35:04 +0800

    ✨ feat: add openai sdk

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,10 @@
-const res = await fetch("http://localhost:11434/api/generate", {
-  method: "POST",
-  body: JSON.stringify({
-    model: "qwen3:4b-instruct-2507-q4_K_M",
-    prompt: "why is the sky blue?",
-    // stream: false,
-  }),
+import OpenAI from "openai";
+
+const openai = new OpenAI({ baseURL: "http://localhost:11434/v1", apiKey: "" });
+
+const res = await openai.responses.create({
+  model: "qwen3:4b-instruct-2507-q4_K_M",
+  input: "hello",
 });
 
-if (!res.body) process.exit(0);
-
-const decoderStream = new TextDecoderStream();
-
-const stream = res.body.pipeThrough(decoderStream);
-
-for await (const chunk of stream) {
-  const json = JSON.parse(chunk);
-  process.stdout.write(json.response);
-}
-
-process.stdout.write("\n");
+console.log(res);
