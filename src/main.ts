a97af97cc01fc9ed0e22d0adfa3f142c34891589
shervinchen <shervinchen@gmail.com>
2026-03-30 13:26:36 +0800

    ✨ feat: add get system date time tool

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
-import { generateText } from "ai";
+import { generateText, stepCountIs, tool } from "ai";
 import { ollama } from "ollama-ai-provider-v2";
+import { z } from "zod";
 
 const res = await generateText({
   model: ollama("qwen3:4b-instruct-2507-q4_K_M"),
-  prompt: "What is the meaning of life?",
+  prompt: "What time is it now?",
+  stopWhen: stepCountIs(3),
+  tools: {
+    get_system_date_time: tool({
+      description:
+        "Return the system current date time. Useful to provide the current time to user.",
+      inputSchema: z.object({}),
+      execute() {
+        return new Date().toLocaleString();
+      },
+    }),
+  },
 });
 
 console.log(res);
